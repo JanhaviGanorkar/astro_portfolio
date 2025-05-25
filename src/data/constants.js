1bb7498d891c34f7b2d677062e1c11bda35f5347
janhaviganorkar <janhaviganorkar12@gmail.com>
2025-05-25 08:48:55 +0530

    fix

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,13 +5,14 @@ export const Bio = {
     "Frontend Web Developer",
     "React Js Developer",
     "Python Developer",
+    "Python Teacher",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/JanhaviGanorkar",
   resume:
-    "https://drive.google.com/file/d/14UgTJBHY36hBnDb2nsEPyKM_27cEq2CI/view",
+   "https://drive.google.com/file/d/19DY7zHVTiym-IuHcSjuT7w2EFEpz4ipZ/view",
   linkedin: "https://www.linkedin.com/in/janhavi-ganorkar-263752294/",
   twitter: "https://x.com/janhaviganorkar",
   insta: "https://www.instagram.com/itz_janhavi_official/",
@@ -23,7 +24,6 @@ export const achievements = [
     title: "Teching Compititon",
     date: "March 2024",
     description: "Won 1st Prize (INR 30,000) for teaching excellence on Campus Code Hero platform",
-    link: "certification-link-here"
   },
 ]
 
@@ -54,32 +54,39 @@ export const projects = [
     description: "Search Engine website where you can save data and perform vector-based searches on it",
     image: "Vector_eng.png",
     tags: ["React.js", "MongoDB", "Express", "Natural", "TF-IDF", "Tailwind CSS"],
-    category: "website",
-    github: "https://github.com/JanhaviGanorkar/Vector_search_fron",
+    github: "https://github.com/jan123GAN/vector_search_frontend",
     link: "https://vector-search-frontend-4fsq.vercel.app/"
   },
-  {
+  // {
+  //   id: 2,
+  //   title: "Chat App",
+  //   date: "March 2024",
+  //   description: "Real-time chat application for connecting with friends, in this website features like Add friend, Edit profile, Login",
+  //   image: "chat-app.png",
+  //   tags: ["Django", "React", "JWT", "Zustand", "Tanstack Query", "WebSocket"],
+  //   github: "https://github.com/JanhaviGanorkar/My_Chat_App",
+  //   link: "https://github.com/JanhaviGanorkar/My_Chat_App"
+  // },
+   {
     id: 2,
-    title: "Chat App",
-    date: "March 2024",
-    description: "Real-time chat application for connecting with friends, in this website features like Add friend, Edit profile, Login",
-    image: "chat-app.png",
-    tags: ["Django", "React", "JWT", "Zustand", "Tanstack Query", "WebSocket"],
-    category: "website",
-    github: "https://github.com/JanhaviGanorkar/My_Chat_App",
-    link: "https://github.com/JanhaviGanorkar/My_Chat_App"
-  },
-  {
-    id: 3,
-    title: "Social Media Website",
+    title: "Ui",
     date: "February 2024",
-    description: "Social media platform with features for Login, posting, liking, sharing content, and Commenting",
+    description: "Animations",
     image: "social.png",
-    tags: ["Django", "HTML", "CSS", "Tailwind", "Jinja Templet",],
-    category: "website",
-    github: "https://github.com/JanhaviGanorkar/pro2-master",
-    link: "https://github.com/JanhaviGanorkar/pro2-master"
+    tags: ["Django", "HTML", "CSS"],
+    github: "https://github.com/JanhaviGanorkar/animated-website",
+    link: "https://janhaviganorkar.github.io/animated-website/"
   }
+  // {
+  //   id: 3,
+  //   title: "Social Media Website",
+  //   date: "February 2024",
+  //   description: "Social media platform with features for Login, posting, liking, sharing content, and Commenting",
+  //   image: "social.png",
+  //   tags: ["Django", "HTML", "CSS", "Tailwind", "Jinja Templet",],
+  //   github: "https://github.com/JanhaviGanorkar/pro2-master",
+  //   link: "https://github.com/JanhaviGanorkar/pro2-master"
+  // }
 ];
 
 export const education = [
@@ -88,7 +95,6 @@ export const education = [
     img: "vmvlogo.jpg",
     school: "Govenment vidarbh institute of science and humanity",
     date: "August 2021 - April 2024",
-    grade: "8.60 CGPA",
     desc: "I completed my Bachelor's degree in Computer Science at Govenment vidarbh institute of science and humanity.",
     degree: "Bachelor of Technology - Bsc",
   },
@@ -97,7 +103,6 @@ export const education = [
     img: "https://sch.edu.in/wp-content/uploads/2021/11/SCHLOGO.png",
     school: "Bhagyashree Vidhayalaya & Junior College Amravati",
     date: "August 2017 - April 2019",
-    grade: "45%",
     desc: "I completed my class 12 high school education at Bhagyashree Vidhayalaya & Junior College Amravati",
     degree: "Intermediate, Science(PCMB)",
   },
@@ -106,7 +111,6 @@ export const education = [
     img: "https://www.nuaodisha.com/images/Contents/NuaOdisha-136370-Saraswati-VidyaMandir.png",
     school: "Bhagyashree Vidhayalaya & Junior College Amravati",
     date: "April 2016 - April 2017",
-    grade: "62.42%",
     desc: "I completed my class 10 education at Bhagyashree Vidhayalaya & Junior College Amravati.",
     degree: "Matriculation",
   },
